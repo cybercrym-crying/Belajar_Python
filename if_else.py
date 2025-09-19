68d17b3bee4959936d9fd9b97df46b167fe3be25
Crym <crymbero@gmail.com>
2025-09-19 17:26:26 +0800

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,8 @@
+nama = input("Masukan Nama Anda : ")
+if nama == "Akbar":
+    # Menjorok ke dalam kode akan ikut if
+    print(f"Nama anda {nama}")
+elif nama == "Adi":
+    print(f"Nama anda bukan Akbar tapi {nama}")
+else:
+    print(f"Anda bukan siapa siapa")
